8ef25930a225433922d5d302043df3fc01af7f1b
SotoiGhost <israel.spgh@gmail.com>
2020-04-28 13:50:40 -0500

    Finished exercise

--- Xylophone/ViewController.swift
+++ Xylophone/ViewController.swift
@@ -7,15 +7,28 @@
 //
 
 import UIKit
+import AVFoundation
 
 class ViewController: UIViewController {
+    
+    var player: AVAudioPlayer!
 
     override func viewDidLoad() {
         super.viewDidLoad()
     }
 
-
+    @IBAction func keyPressed(_ sender: UIButton) {
+        let soundName = sender.currentTitle!
+        playSound(soundName: soundName)
+        sender.alpha = 0.5
+        Timer.scheduledTimer(withTimeInterval: 0.2, repeats: false) { _ in
+            sender.alpha = 1
+        }
+    }
     
-
+    func playSound(soundName: String) {
+        let url = Bundle.main.url(forResource: soundName, withExtension: "wav")
+        player = try! AVAudioPlayer(contentsOf: url!)
+        player.play()
+    }
 }
-
